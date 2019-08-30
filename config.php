8c32170b7d8b5d867a158a2e30fb645f7fb28e49
Basoro <dentix.id@gmail.com>
2019-08-30 08:50:32 +0800

    Update

--- config.php
+++ config.php
@@ -13,7 +13,7 @@
 
 if (preg_match ('/config.php/', basename($_SERVER['PHP_SELF']))) die ('Unable to access this script directly from browser!');
 
-define('VERSION', '0.1 Beta');
+define('VERSION', '1.0');
 define('ABSPATH', dirname(__FILE__) . '/');
 define('URL', 'http://localhost/dashboard');
 define('URLSIMRS', 'http://localhost/dashboard');
