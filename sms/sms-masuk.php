d931dfab1b942395cdb65dd460f4e87fee3b08eb
Basoro <dentix.id@gmail.com>
2019-09-01 18:40:19 +0800

    Upate

--- sms/sms-masuk.php
+++ sms/sms-masuk.php
@@ -1,6 +1,6 @@
 <?php
 /***
-* SIMRS Khanza Lite from version 0.1 Beta
+* SIMRS Khanza Lite from version 1.0
 * About : Porting of SIMRS Khanza by Windiarto a.k.a Mas Elkhanza as web and mobile app.
 * Last modified: 02 Pebruari 2018
 * Author : drg. Faisol Basoro
