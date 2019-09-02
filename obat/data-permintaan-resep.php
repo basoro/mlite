647486dae329d8a514107d1302d75a12976d7206
Basoro <dentix.id@gmail.com>
2019-09-02 17:10:39 +0800

    Update

--- obat/data-permintaan-resep.php
+++ obat/data-permintaan-resep.php
@@ -9,9 +9,9 @@
 ***/
 
 $title = 'Data Permintaan Resep';
-include_once('config.php');
-include_once('layout/header.php');
-include_once('layout/sidebar.php');
+include_once('../config.php');
+include_once('../layout/header.php');
+include_once('../layout/sidebar.php');
 
 		?>
 
@@ -26,18 +26,18 @@ include_once('layout/sidebar.php');
                             </h2>
                         </div>
                         <div class="body">
-						  <?php 
+						  <?php
 							  $action = isset($_GET['action'])?$_GET['action']:null;
 							  if(!$action){
 							?>
 							<form method="POST" action="" enctype="multipart/form-data">
-							<div class="form-group form-float">							
+							<div class="form-group form-float">
 								<select name="status" style="width:100%" class="form-control kd_tdk">
 									<option value="Rajal" selected="selected" >Rawat Jalan</option>
 									<option value="Ranap">Rawat Inap</option>
 									<option value="IGDK" >IGD</option>
 									<option value="U0019" >Hemodialisa</option>
-								</select>	
+								</select>
 							</div>
 							<div class="form-group form-float">
 								<div class="form-line">
@@ -50,8 +50,8 @@ include_once('layout/sidebar.php');
 									<label class="form-label">Tanggal Akhir</label>
 									<input type="text" name="tanggal_akhir" class="datepicker form-control" value="<?php echo date('Y-m-d');?>">
 								</div>
-							</div>	
-							<button type="submit" class='btn btn-block btn-lg btn-info waves-effect'> 
+							</div>
+							<button type="submit" class='btn btn-block btn-lg btn-info waves-effect'>
 								Cari Data Resep
 							</button>
 							</form> <br><br>
@@ -66,7 +66,7 @@ include_once('layout/sidebar.php');
 								<th>Nama Pasien</th>
 								<th>Nama Dokter</th>
 								<th>Ruang</th>
-								<th>Status</th>  
+								<th>Status</th>
 							</tr>
 							</thead>
 							<tbody>
@@ -83,10 +83,10 @@ include_once('layout/sidebar.php');
 									if ($status == "Rajal"){
 										$query = "SELECT a.no_resep,a.tgl_perawatan,a.no_rawat,b.no_rkm_medis,c.nm_pasien,d.nm_dokter,a.tgl_peresepan, e.nm_poli,a.status as stts,
 												if (a.jam_peresepan=a.jam,'Belum Terlayani','Sudah Terlayani') as status FROM resep_obat as a,reg_periksa as b,
-												pasien as c,dokter as d,poliklinik as e where a.tgl_peresepan >='$tgl_awal' and a.tgl_peresepan <= '$tgl_akhir' 
+												pasien as c,dokter as d,poliklinik as e where a.tgl_peresepan >='$tgl_awal' and a.tgl_peresepan <= '$tgl_akhir'
 												AND a.no_rawat=b.no_rawat AND b.no_rkm_medis=c.no_rkm_medis and b.kd_poli=e.kd_poli and b.kd_poli NOT IN ('IGDK','U0027','U0019')
 												AND a.kd_dokter = d.kd_dokter";
-										$execute=query($query); 
+										$execute=query($query);
 										while ($row = fetch_array($execute)){
 										?>
 										  <tr>
@@ -97,24 +97,24 @@ include_once('layout/sidebar.php');
 											  <td><?php echo $row['nm_pasien'];?></td>
 											  <td><?php echo $row['nm_dokter'];?></td>
 											  <td><?php echo $row['nm_poli'];?></td>
-											  <td><?php echo $row['status'];?></td>  
+											  <td><?php echo $row['status'];?></td>
 										  </tr>
 										  <?php
 										}
 									}
 									if ($status == "Ranap"){
-										$query = "SELECT a.no_resep,a.tgl_peresepan, a.no_rawat,b.no_rkm_medis, c.nm_pasien, if (a.jam_peresepan=a.jam,'Belum Terlayani','Sudah Terlayani') as status, 
+										$query = "SELECT a.no_resep,a.tgl_peresepan, a.no_rawat,b.no_rkm_medis, c.nm_pasien, if (a.jam_peresepan=a.jam,'Belum Terlayani','Sudah Terlayani') as status,
 												a.status, f.nm_bangsal, g.nm_dokter FROM resep_obat as a, reg_periksa as b, pasien as c,kamar_inap as d,
-												kamar as e,bangsal as f, dokter as g where a.tgl_peresepan >='$tgl_awal'  and a.tgl_peresepan <= '$tgl_akhir' 
+												kamar as e,bangsal as f, dokter as g where a.tgl_peresepan >='$tgl_awal'  and a.tgl_peresepan <= '$tgl_akhir'
 												AND a.status = 'ranap' AND a.no_rawat = b.no_rawat AND b.no_rawat = d.no_rawat AND a.kd_dokter = g.kd_dokter AND
 												d.kd_kamar = e.kd_kamar AND e.kd_bangsal = f.kd_bangsal AND b.no_rkm_medis = c.no_rkm_medis";
-										
+
 										//$query = "SELECT a.no_resep,a.tgl_perawatan,a.no_rawat,b.no_rkm_medis,c.nm_pasien,d.nm_dokter,a.tgl_peresepan, e.nm_poli,a.status as stts,
 										//			if (a.jam_peresepan=a.jam,'Belum Terlayani','Sudah Terlayani') as status FROM resep_obat as a,reg_periksa as b,
-										//			pasien as c,dokter as d,poliklinik as e where a.tgl_peresepan >='$tgl_awal' and a.tgl_peresepan <= '$tgl_akhir' 
+										//			pasien as c,dokter as d,poliklinik as e where a.tgl_peresepan >='$tgl_awal' and a.tgl_peresepan <= '$tgl_akhir'
 										//			AND a.no_rawat=b.no_rawat AND b.no_rkm_medis=c.no_rkm_medis and b.kd_poli=e.kd_poli
 										//			AND stts ='ranap' AND a.kd_dokter = d.kd_dokter";
-													$execute=query($query); 
+													$execute=query($query);
 													while ($row = fetch_array($execute)){
 													?>
 													  <tr>
@@ -125,7 +125,7 @@ include_once('layout/sidebar.php');
 														  <td><?php echo $row['nm_pasien'];?></td>
 														  <td><?php echo $row['nm_dokter'];?></td>
 														  <td><?php echo $row['nm_bangsal'];?></td>
-														  <td><?php echo $row['status'];?></td>  
+														  <td><?php echo $row['status'];?></td>
 													  </tr>
 										  <?php
 										}
@@ -133,10 +133,10 @@ include_once('layout/sidebar.php');
 									if ($status == "IGDK"){
 										$query = "SELECT a.no_resep,a.tgl_perawatan,a.no_rawat,b.no_rkm_medis,c.nm_pasien,d.nm_dokter,a.tgl_peresepan, e.nm_poli,a.status as stts,
 													if (a.jam_peresepan=a.jam,'Belum Terlayani','Sudah Terlayani') as status FROM resep_obat as a,reg_periksa as b,
-													pasien as c,dokter as d,poliklinik as e where a.tgl_peresepan >='$tgl_awal' and a.tgl_peresepan <= '$tgl_akhir' 
+													pasien as c,dokter as d,poliklinik as e where a.tgl_peresepan >='$tgl_awal' and a.tgl_peresepan <= '$tgl_akhir'
 													AND a.no_rawat=b.no_rawat AND b.no_rkm_medis=c.no_rkm_medis and b.kd_poli=e.kd_poli
 													AND e.kd_poli = 'IGDK' AND a.kd_dokter = d.kd_dokter GROUP BY a.no_rawat";
-													$execute=query($query); 
+													$execute=query($query);
 													while ($row = fetch_array($execute)){
 													?>
 													  <tr>
@@ -147,7 +147,7 @@ include_once('layout/sidebar.php');
 														  <td><?php echo $row['nm_pasien'];?></td>
 														  <td><?php echo $row['nm_dokter'];?></td>
 														  <td><?php echo $row['nm_poli'];?></td>
-														  <td><?php echo $row['status'];?></td>  
+														  <td><?php echo $row['status'];?></td>
 													  </tr>
 										  <?php
 										}
@@ -155,10 +155,10 @@ include_once('layout/sidebar.php');
 									if ($status == "U0019"){
 											$query = "SELECT a.no_resep,a.tgl_perawatan,a.no_rawat,b.no_rkm_medis,c.nm_pasien,d.nm_dokter,a.tgl_peresepan, e.nm_poli,a.status as stts,
 													if (a.jam_peresepan=a.jam,'Belum Terlayani','Sudah Terlayani') as status FROM resep_obat as a,reg_periksa as b,
-													pasien as c,dokter as d,poliklinik as e where a.tgl_peresepan >='$tgl_awal' and a.tgl_peresepan <= '$tgl_akhir' 
+													pasien as c,dokter as d,poliklinik as e where a.tgl_peresepan >='$tgl_awal' and a.tgl_peresepan <= '$tgl_akhir'
 													AND a.no_rawat=b.no_rawat AND b.no_rkm_medis=c.no_rkm_medis and b.kd_poli=e.kd_poli
 													AND e.kd_poli = 'U0019' AND a.kd_dokter = d.kd_dokter";
-													$execute=query($query); 
+													$execute=query($query);
 													while ($row = fetch_array($execute)){
 													?>
 													  <tr>
@@ -169,7 +169,7 @@ include_once('layout/sidebar.php');
 														  <td><?php echo $row['nm_pasien'];?></td>
 														  <td><?php echo $row['nm_dokter'];?></td>
 														  <td><?php echo $row['nm_poli'];?></td>
-														  <td><?php echo $row['status'];?></td>  
+														  <td><?php echo $row['status'];?></td>
 													  </tr>
 										  <?php
 										}
@@ -178,9 +178,9 @@ include_once('layout/sidebar.php');
 								?>
 							</tbody>
 						</table>
-							<?php  
-							
-							}  
+							<?php
+
+							}
 						  ?>
                         </div>
                     </div>
@@ -190,5 +190,5 @@ include_once('layout/sidebar.php');
     </section>
 
 <?php
-include_once('layout/footer.php');
+include_once('../layout/footer.php');
 ?>
