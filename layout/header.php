5da8c6297ef1cf5d9f1aef6307ad59df17a030a0
Basoro <dentix.id@gmail.com>
2019-10-07 08:50:47 +0800

    Update

--- layout/header.php
+++ layout/header.php
@@ -65,17 +65,6 @@ include_once('init.php');
     <!-- Overlay For Sidebars -->
     <div class="overlay"></div>
     <!-- #END# Overlay For Sidebars -->
-    <!-- Search Bar -->
-    <div class="search-bar">
-        <div class="search-icon">
-            <i class="material-icons">search</i>
-        </div>
-        <input type="text" placeholder="START TYPING...">
-        <div class="close-search">
-            <i class="material-icons">close</i>
-        </div>
-    </div>
-    <!-- #END# Search Bar -->
     <!-- Top Bar -->
     <nav class="navbar">
         <div class="container-fluid">
