4ddeefc881aa8efffb29cf5bd4da5dad881221c9
Basoro <dentix.id@gmail.com>
2019-10-22 08:41:02 +0800

    Update pendaftaran pasien isian kelurahan

---
+++ includes/data-kelurahan.php
@@ -0,0 +1,47 @@
+<?php
+
+/***
+* SIMRS Khanza Lite from version 0.1 Beta
+* About : Porting of SIMRS Khanza by Windiarto a.k.a Mas Elkhanza as web and mobile app.
+* Last modified: 02 Pebruari 2018
+* Author : drg. Faisol Basoro
+* Email : dentix.id@gmail.com
+* Licence under GPL
+***/
+
+ob_start();
+session_start();
+
+include ('../config.php');
+
+$page = isset($_GET['p'])? $_GET['p'] : '';
+
+if($page=='add'){
+  if(!empty($_POST['nama_kelurahan'])){
+    $insert = query("INSERT INTO kelurahan (kd_kel, nm_kel) VALUES (NULL, '{$_POST['nama_kelurahan']}')");
+  }
+} else {
+ ?>
+<table id="kelurahan" class="table responsive table-bordered table-striped table-hover display nowrap" width="100%">
+    <thead>
+        <tr>
+            <th>Kode Kelurahan</th>
+            <th>Nama Kelurahan</th>
+        </tr>
+    </thead>
+    <tbody>
+      <?php
+      $sql_kelurahan = "SELECT * FROM kelurahan";
+      $result_kelurahan = query($sql_kelurahan);
+      while($row = fetch_array($result_kelurahan)) {
+        echo '<tr class="pilihkelurahan" data-kdkel='.$row[0].' data-nmkel='.$row[1].'>';
+        echo '<td>'.$row[0].'</td>';
+        echo '<td>'.$row[1].'</td>';
+        echo '</tr>';
+      }
+      ?>
+    </tbody>
+</table>
+<?php
+}
+?>
