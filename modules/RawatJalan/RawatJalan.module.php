31784949e86f1e26293e909abed5de9440e284d6
Basoro <dentix.id@gmail.com>
2019-10-27 09:25:41 +0800

    Update Module RawatJalan

--- modules/RawatJalan/RawatJalan.module.php
+++ modules/RawatJalan/RawatJalan.module.php
@@ -5,6 +5,7 @@ if(!defined('IS_IN_MODULE')) { die("NO DIRECT FILE ACCESS!"); }
 <?php
 class RawatJalan {
     function index() {
+        global $dataSettings, $date;
        include('modules/RawatJalan/pasien.php');
     }
     function rujuk_internal() {
