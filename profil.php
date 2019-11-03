6630ac02cf4befdbdc79e70476edff89c6f51605
Basoro <dentix.id@gmail.com>
2019-11-03 09:51:50 +0800

    Update

--- profil.php
+++ profil.php
@@ -150,7 +150,7 @@ $b = fetch_assoc($a);
                                         <div class="col-sm-4">
                                             <div class="form">
                                               <?php if($b['photo'] !==''){ ?>
-                                                <img id="image_upload_preview" width="200px" src="<?php echo URLSIMRS; ?>/penggajian/pages/pegawai/photo/<?php echo $b['photo']; ?>" onclick="upload_berkas()" style="cursor:pointer;" />
+                                                <img id="image_upload_preview" width="200px" src="<?php echo URLSIMRS; ?>/penggajian/<?php echo $b['photo']; ?>" onclick="upload_berkas()" style="cursor:pointer;" />
                                               <?php } else { ?>
                                                 <img id="image_upload_preview" width="200px" src="<?php echo URL; ?>/assets/images/<?php echo $dataGet['1']; ?>.png" onclick="upload_berkas()" style="cursor:pointer;" />';
                                               <?php } ?>
