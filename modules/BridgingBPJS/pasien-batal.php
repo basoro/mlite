84002352ccde760fce303f33a0110aae3ae9f170
Basoro <dentix.id@gmail.com>
2019-11-04 17:26:44 +0800

    Update

--- modules/BridgingBPJS/pasien-batal.php
+++ modules/BridgingBPJS/pasien-batal.php
@@ -19,16 +19,22 @@
         <?php $hapus = query("SELECT reg_periksa.no_rkm_medis , pasien.nm_pasien , poliklinik.nm_poli , reg_periksa.tgl_registrasi , reg_periksa.no_rawat , pasien.no_peserta
         FROM reg_periksa , poliklinik , pasien WHERE reg_periksa.kd_poli = poliklinik.kd_poli AND
         reg_periksa.no_rkm_medis = pasien.no_rkm_medis AND reg_periksa.stts = 'Batal' AND reg_periksa.kd_pj IN ('A02','BPJ') AND reg_periksa.tgl_registrasi = CURRENT_DATE()");
-              while($row = fetch_array($hapus)) {?>
-          <tr>
-            <td><?php echo $row['no_rkm_medis'];?></td>
-            <td><?php echo SUBSTR($row['nm_pasien'], 0, 15).' ...';?></td>
-            <td><?php echo $row['nm_poli'];?></td>
-            <td><?php echo $row['tgl_registrasi'];?></td>
-            <td><?php echo $row['no_peserta'];?></td>
-            <td><?php $sep = fetch_array(query("SELECT no_sep from bridging_sep where no_rawat = '".$row['no_rawat']."'"));echo $sep['no_sep'];?></td>
-            <td><a href="hapus-brid.php?no_sep=<?php echo $sep['no_sep'];?>" class="btn btn-danger">X</a></td>
-          </tr><?php } ?>
+              while($row = fetch_array($hapus)) {
+                if($sep['no_sep'] !== "") {
+                ?>
+                <tr>
+                  <td><?php echo $row['no_rkm_medis'];?></td>
+                  <td><?php echo SUBSTR($row['nm_pasien'], 0, 15).' ...';?></td>
+                  <td><?php echo $row['nm_poli'];?></td>
+                  <td><?php echo $row['tgl_registrasi'];?></td>
+                  <td><?php echo $row['no_peserta'];?></td>
+                  <td><?php $sep = fetch_array(query("SELECT no_sep from bridging_sep where no_rawat = '".$row['no_rawat']."'"));echo $sep['no_sep'];?></td>
+                  <td><a href="hapus-brid.php?no_sep=<?php echo $sep['no_sep'];?>" class="btn btn-danger">X</a></td>
+                </tr>
+                <?php
+                }
+              }
+              ?>
         </tbody>
       </table>
     </div>
