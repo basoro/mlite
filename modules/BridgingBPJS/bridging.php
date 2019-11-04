559bef8b02c5322c9409a8b001481fb5a9c8d4ae
Basoro <dentix.id@gmail.com>
2019-11-04 17:13:03 +0800

    Update

--- modules/BridgingBPJS/bridging.php
+++ modules/BridgingBPJS/bridging.php
@@ -163,7 +163,7 @@ if($role == 'Medis' || $role == 'Paramedis') {
         '{$ktrk}','{$_POST['tglkkl']}','{$_POST['ktrg']}','{$supl}','{$_POST['sepsup']}','','','','','','','{$_POST['skdp']}','{$_POST['dpjp']}','{$_POST['nmdpjp']}')");
     	}else {
       ?>
-        <script>alert('<?php echo "Kode : ".$meta."\nPesan : ".$mets; ?>')</script>
+        <script>alert('<?php echo "(".$meta.") Pesan : ".$mets; ?>')</script>
       <?php
       };
     }
