21ac67ae3d4cab7c189857ad62deb672515a9b61
Basoro <dentix.id@gmail.com>
2019-11-05 22:24:48 +0800

    Update

--- modules/Farmasi/includes/select-obat.php
+++ modules/Farmasi/includes/select-obat.php
@@ -9,11 +9,15 @@
 * Licence under GPL
 ***/
 
+if(!isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
+   header("HTTP/1.0 403 Forbidden");
+   exit;
+}
+
 ob_start();
 session_start();
 
-include ('../config.php');
-include ('../init.php');
+include ('../../../config.php');
 
 $q = $_GET['q'];
 
