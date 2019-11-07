e3250e9dd635c83f25ab8cfc4c8e0bed50e1398c
Basoro <dentix.id@gmail.com>
2019-11-08 05:21:14 +0800

    Update

--- print_billing.php
+++ print_billing.php
@@ -31,7 +31,7 @@ if(isset($_GET['no_rawat'])) {
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="https://adminlte.io/themes/AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo URL; ?>/assets/plugins/bootstrap/css/bootstrap.min.css">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo URL; ?>/assets/plugins/font-awesome/css/font-awesome.min.css">
   <!-- Theme style -->
