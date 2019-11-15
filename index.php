fc2778b3a1d9fb9e7aadae110cc04f2e775a24d1
Basoro <dentix.id@gmail.com>
2019-11-15 12:17:29 +0800

    Update

--- index.php
+++ index.php
@@ -168,6 +168,21 @@ include_once('layout/sidebar.php');
         </div>
     </section>
 <?php } ?>
+
+<div class="modal hide in" id="pengumuman">
+  <div class="modal-header">
+    <a class="close" data-dismiss="modal">×</a>
+    <h3>Modal header</h3>
+  </div>
+  <div class="modal-body">
+    <p>One fine body…</p>
+  </div>
+  <div class="modal-footer">
+    <a href="#" class="btn">Close</a>
+    <a href="#" class="btn btn-primary">Save changes</a>
+  </div>
+</div>
+
 <?php include_once('layout/footer.php'); ?>
 <?php if(!$getmodule) { ?>
     <script>
@@ -281,17 +296,3 @@ include_once('layout/sidebar.php');
 
     </script>
 <?php } ?>
-
-<div class="modal hide in" id="pengumuman">
-  <div class="modal-header">
-    <a class="close" data-dismiss="modal">×</a>
-    <h3>Modal header</h3>
-  </div>
-  <div class="modal-body">
-    <p>One fine body…</p>
-  </div>
-  <div class="modal-footer">
-    <a href="#" class="btn">Close</a>
-    <a href="#" class="btn btn-primary">Save changes</a>
-  </div>
-</div>
