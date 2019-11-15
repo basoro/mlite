6067df61636b716a42473fedef4a1171e98a6b9b
Basoro <dentix.id@gmail.com>
2019-11-15 12:50:36 +0800

    Update

--- index.php
+++ index.php
@@ -133,7 +133,7 @@ include_once('layout/sidebar.php');
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="unitModalLabel">Quisioner Pelayanan Medik</h4>
+                    <h4 class="modal-title" id="unitModalLabel">Quisioner</h4>
                 </div>
                 <div class="modal-body">
                 </div>
