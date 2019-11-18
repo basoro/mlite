a475583974f6b5cdfd747430fd469b658f3ceeae
Basoro <dentix.id@gmail.com>
2019-11-19 00:02:14 +0800

    Update

--- modules/Survei/Survei.module.php
+++ modules/Survei/Survei.module.php
@@ -5,7 +5,7 @@ if(!defined('IS_IN_MODULE')) { die("NO DIRECT FILE ACCESS!"); }
 <?php
 class Survei {
     function index() {
-      include('modules/PMKP/dashboard.php');
+      include('modules/Survei/dashboard.php');
     }
 }
 ?>
