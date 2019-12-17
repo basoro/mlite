21aff5cd7721ff4f2d28a4388de39128dae8cbaf
Basoro <dentix.id@gmail.com>
2019-12-18 04:23:23 +0800

    Update

--- layout/footer.php
+++ layout/footer.php
@@ -1,7 +1,7 @@
 
     <!--
     Sebagai respect kepada TIM Pengembangan SIMRS Khanza Lite RSUD H. Damanhuri Barabai,
-    atas effort dan kerja keras temen-temen di Instalasi ICT, 
+    atas effort dan kerja keras temen-temen di Instalasi ICT,
     kami harap tidak menghilangkan modal content dibawah ini.
     Jika ingin mengganti, memodifikasi, diperbolehkan dengan tetap menyertakannya.
     Terima kasih atas pengertian anda.
@@ -170,7 +170,7 @@
 
         $('.count-to').countTo();
 
-      	$('#aniimated-thumbnials').lightGallery({
+      	$('#animated-thumbnails').lightGallery({
           	thumbnail: true,
           	selector: 'a'
       	});
