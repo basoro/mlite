a17251eb4fdb98edc73db0fd9a50fb06c530d267
Basoro <dentix.id@gmail.com>
2019-12-20 17:33:00 +0800

    Update

--- config.php
+++ config.php
@@ -29,7 +29,7 @@ define('KODEPROP','63prop');
 define('IS_IN_MODULE', true);
 define('FKTL', false);
 define('WEBAPPS', '../webapps');
-define('THEME', 'indigo'); // amber, black, blue, blue-grey, brown, cyan, deep-orange, deep-purple, green, grey, indigo, light-blue, lime, orange, pink, purple, red, teal, yellow
+define('THEME', 'deep-purple'); // amber, black, blue, blue-grey, brown, cyan, deep-orange, deep-purple, green, grey, indigo, light-blue, lime, orange, pink, purple, red, teal, yellow
 define('PWA', false);
 
 define('BpjsApiUrl', 'https://new-api.bpjs-kesehatan.go.id:8080/new-vclaim-rest/');
