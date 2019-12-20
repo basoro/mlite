49687d17363a409be78af9dde3af2bc7685293a7
adlylee <adityahidayat@gmail.com>
2019-12-21 07:46:59 +0800

    survei infeksi

---
+++ modules/RawatInap/includes/toperasi.php
@@ -0,0 +1,69 @@
+                                <form action="" method="post">
+                                    <div class="row clearfix">
+                                        <div class="col-sm-12">
+                                            <div class="form-group">
+                                                <div class="form-line">
+                                                <label for="diagnosa">Diagnosa</label><input type="text" placeholder="" name="diagnosa" class="form-control" id="lokasi">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row clearfix">
+                                        <div class="col-sm-3">
+                                            <div class="form-group">
+                                                <div class="form-line">
+                                                <label for="top">Tanggal Operasi</label><input type="text" placeholder="" name="top" class="form-control datepicker" id="mulai">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-2">
+                                        <label for="top">Lama Operasi</label>
+                                            <div class="form-group">
+                                                <div class="form-line">
+                                                <input type="number" placeholder="jam" name="top" class="form-control" id="mulai">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-2">
+                                        <label for="top"> </label>
+                                            <div class="form-group">
+                                                <div class="form-line">
+                                                <input type="number" placeholder="menit" name="top" class="form-control" id="mulai">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <label for="">Jenis Operasi</label>
+                                    <div class="demo-checkbox">
+                                        <input type="checkbox" name="bersih" id="cekbox1">
+                                        <label for="cekbox1">Bersih</label>
+                                        <input type="checkbox" name="bersihcemar" id="cekbox2">
+                                        <label for="cekbox2">Bersih Tercemar</label>
+                                        <input type="checkbox" name="cemar" id="cekbox3">
+                                        <label for="cekbox3">Tercemar</label>
+                                        <input type="checkbox" name="kotor" id="cekbox4">
+                                        <label for="cekbox4">Kotor</label>
+                                    </div>
+                                    <label for="">Tindakan Operasi</label>
+                                    <div class="demo-checkbox">
+                                        <input type="checkbox" name="vensen" id="cekbox5">
+                                        <label for="cekbox5">Cito</label>
+                                        <input type="checkbox" name="venper" id="cekbox6">
+                                        <label for="cekbox6">Elektif</label>
+                                    </div>
+                                    <label for="">ASA Score</label>
+                                    <div class="demo-checkbox">
+                                        <input type="checkbox" name="bersih" id="asa1">
+                                        <label for="asa1">1</label>
+                                        <input type="checkbox" name="bersihcemar" id="asa2">
+                                        <label for="asa2">2</label>
+                                        <input type="checkbox" name="cemar" id="asa3">
+                                        <label for="asa3">3</label>
+                                        <input type="checkbox" name="kotor" id="asa4">
+                                        <label for="asa4">4</label>
+                                        <input type="checkbox" name="kotor" id="asa5">
+                                        <label for="asa5">5</label>
+                                    </div>
+                                    
+                                </form>
