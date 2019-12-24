818f0c4b0f352c03829d6ab23dca81a0b3d57647
Basoro <dentix.id@gmail.com>
2019-12-25 06:49:38 +0800

    Update

--- modules/Umpeg/Umpeg.module.php
+++ modules/Umpeg/Umpeg.module.php
@@ -14,22 +14,22 @@ class Umpeg {
     <table id="datatable" class="table table-bordered table-striped table-hover display nowrap" width="100%">
         <thead>
             <tr>
-                <th>Nomor Induk</th>
                 <th>Nama</th>
+                <th>Nomor Induk</th>
                 <th>Jenis Kelamin</th>
                 <th>Jabatan</th>
                 <th>Bidang</th>
             </tr>
         </thead>
         <tbody>
-        <?php 
+        <?php
             $sql = "SELECT * from pegawai";
             $query = query($sql);
             while($row = fetch_array($query)) {
         ?>
             <tr>
-                <td><?php echo $row['nik'];?></td>
                 <td><?php echo $row['nama'];?></td>
+                <td><?php echo $row['nik'];?></td>
                 <td><?php echo $row['jk'];?></td>
                 <td><?php echo $row['jbtn'];?></td>
                 <td><?php echo $row['bidang'];?></td>
