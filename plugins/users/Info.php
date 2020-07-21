7fea70de6630344a215b534acaf5c57ef3f4037e
Basoro <dentix.id@gmail.com>
2020-07-21 15:51:30 +0800

    Update Info.php

--- plugins/users/Info.php
+++ plugins/users/Info.php
@@ -18,17 +18,29 @@ return [
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
 
         $core->db()->pdo()->exec("CREATE TABLE IF NOT EXISTS `lite_login_attempts` (
-            `ip`    TEXT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            `attempts`  int(11) NOT NULL,
-            `expires`   int(11) NOT NULL DEFAULT '0'
+            `ip` TEXT NOT NULL,
+            `attempts` int(11) NOT NULL,
+            `expires` int(11) NOT NULL DEFAULT '0'
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
 
         $core->db()->pdo()->exec("CREATE TABLE IF NOT EXISTS `lite_remember_me` (
-            `id` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            `id` int(10) NOT NULL,
             `token` text NOT NULL,
             `user_id` varchar(50) NOT NULL,
             `expiry` int(11) NOT NULL
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+
+        $core->db()->pdo()->exec('ALTER TABLE `lite_remember_me`
+            ADD PRIMARY KEY (`id`);');
+
+        $core->db()->pdo()->exec('ALTER TABLE `lite_roles`
+            ADD PRIMARY KEY (`id`);');
+
+        $core->db()->pdo()->exec('ALTER TABLE `lite_remember_me`
+            MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;');
+
+        $core->db()->pdo()->exec('ALTER TABLE `lite_roles`
+            MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;');
 
         $row = $this->db()->pdo()->prepare("SELECT AES_DECRYPT(usere,'nur') as username FROM admin");
         $row->execute();
