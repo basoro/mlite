7d6a35dacd680403232c2888123512cc16bb010b
Basoro <dentix.id@gmail.com>
2020-07-22 15:34:20 +0800

    Update Info.php

--- plugins/anjungan/Info.php
+++ plugins/anjungan/Info.php
@@ -10,6 +10,15 @@ return [
     'install'       =>  function () use ($core) {
       $core->db()->pdo()->exec("INSERT INTO `lite_options` (`module`, `field`, `value`) VALUES ('anjungan', 'display_poli', '')");
       $core->db()->pdo()->exec("INSERT INTO `lite_options` (`module`, `field`, `value`) VALUES ('anjungan', 'carabayar_umum', '')");
+      $core->db()->pdo()->exec("CREATE TABLE IF NOT EXISTS `lite_antrian_loket` (
+        `kd` int(50) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `type` varchar(50) NOT NULL,
+        `noantrian` varchar(50) NOT NULL,
+        `postdate` date NOT NULL,
+        `start_time` time NOT NULL,
+        `end_time` time NOT NULL DEFAULT '00:00:00'
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+
     },
     'uninstall'     =>  function () use ($core) {
       $core->db()->pdo()->exec("DELETE FROM `lite_options` WHERE `module` = 'anjungan'");
