fcf0afe38e2cbae2bce8e0e8eccc9524dd6ba7a3
Basoro <dentix.id@gmail.com>
2020-09-26 16:51:47 +0800

    Update

--- plugins/kepegawaian/js/admin/kepegawaian.js
+++ plugins/kepegawaian/js/admin/kepegawaian.js
@@ -16,8 +16,10 @@ $( function() {
   });
 } );
 $(document).ready(function(){
-    $('.display').DataTable({
-      "lengthChange": false,
-      "scrollX": true
-    });
+  $('.display').DataTable({
+    "language": { "search": "", "searchPlaceholder": "Search..." },
+    "lengthChange": false,
+    "scrollX": true,
+    dom: "<<'data-table-title'><'datatable-search'f>><'row'<'col-sm-12'tr>><<'pmd-datatable-pagination' l i p>>"
+  });
 });
