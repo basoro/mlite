778a9f7875bfbf7922048a54ea24c115df617e5a
Basoro <dentix.id@gmail.com>
2020-10-03 19:46:45 +0800

    Rename module kasir_ralan to kasir

---
+++ plugins/kasir/Info.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'name'          =>  'Kasir',
+    'description'   =>  'Pengelolaan data pembayaran.',
+    'author'        =>  'Basoro',
+    'version'       =>  '1.0',
+    'compatibility' =>  '3.*',
+    'icon'          =>  'shopping-basket',
+];
