6467dfca2b858a67d6e1dbaa9dce2e0fa40b24ec
Basoro <dentix.id@gmail.com>
2020-10-07 19:12:48 +0800

    Add PWA Capability

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+
+const cacheName = 'cache-v2';
+const precacheResources = [
+  '/',
+  'js/index.js',
+  'css/style.min.css',
+];
+
+self.addEventListener('install', event => {
+  console.log('Service worker install event!');
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(precacheResources);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activate event!');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(caches.match(event.request)
+    .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+    );
+});
