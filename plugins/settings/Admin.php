54f6879c9c281e0aa0924aa5f1b575637a4f2171
basoro <dentix.id@gmail.com>
2021-01-02 06:41:54 +0800

    Update modul pengaturan
    Catatan perubahan
    - Refractoring ke versi baru 2021
    - Pilhan tema admin
    - Support mobile APPs via PWA
    - Perbaikan modul Kasir Ralan
    - Perbaikan modul Apotik Ralan
    - Perbaikan modul pengaturan
    - Dan masih banyak lagi

--- plugins/settings/Admin.php
+++ plugins/settings/Admin.php
@@ -27,10 +27,21 @@ class Admin extends AdminModule
     public function navigation()
     {
         return [
+            'Pengaturan'          => 'manage',
             'Umum'          => 'general',
             'Tema' => 'theme',
             'Pembaruan'          => 'updates',
         ];
+    }
+
+    public function getManage()
+    {
+      $sub_modules = [
+        ['name' => 'Pengaturan Umum', 'url' => url([ADMIN, 'settings', 'general']), 'icon' => 'wrench', 'desc' => 'Pengaturan umum KhanzaLITE'],
+        ['name' => 'Tema Publik', 'url' => url([ADMIN, 'settings', 'theme']), 'icon' => 'cubes', 'desc' => 'Pengaturan tema tampilan publik'],
+        ['name' => 'Pembaruan Sistem', 'url' => url([ADMIN, 'settings', 'updates']), 'icon' => 'cubes', 'desc' => 'Pembaruan sistem'],
+      ];
+      return $this->draw('manage.html', ['sub_modules' => $sub_modules]);
     }
 
     public function getGeneral()
