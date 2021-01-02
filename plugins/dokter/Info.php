865c2a05169643827624bf6ff479974b86bb1cee
basoro <dentix.id@gmail.com>
2021-01-02 14:43:49 +0800

    UPDATE
    Catatan perubahan
    - Refractoring ke versi baru 2021
    - Pilhan tema admin
    - Support mobile APPs via PWA
    - Perbaikan modul Kasir Ralan
    - Perbaikan modul Apotik Ralan
    - Perbaikan modul pengaturan
    - Dan masih banyak lagi

--- plugins/dokter/Info.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'name'          =>  'Dokter',
-    'description'   =>  'Pengelolaan data pasien untuk dokter.',
-    'author'        =>  'Basoro',
-    'version'       =>  '1.0',
-    'compatibility' =>  '2021',
-    'icon'          =>  'user-md',
-];
