465b9ec7afa4f9798880549dd7a8c661c80a6bed
basoro <dentix.id@gmail.com>
2021-01-04 22:55:14 +0800

    Add module laporan
    Catatan perubahan
    - Refractoring ke versi baru 2021
    - Pilhan tema admin
    - Support mobile APPs via PWA
    - Perbaikan modul Kasir Ralan
    - Perbaikan modul Apotik Ralan
    - Perbaikan modul pengaturan
    - Perbaikan modul dokter rawat jalan
    - Add modul laporan
    - Dan masih banyak lagi

--- plugins/laporan/Admin.php
+++ plugins/laporan/Admin.php
@@ -2,16 +2,9 @@
     namespace Plugins\Laporan;
 
     use Systems\AdminModule;
-    use Plugins\Master\Src\BridgingSEP;
-    use Systems\Lib\Fpdf\PDF_MC_Table;
 
     class Admin extends AdminModule
     {
-
-        public function init()
-        {
-            $this->bridgingsep = new BridgingSEP();
-        }
 
         public function navigation()
         {
@@ -29,75 +22,76 @@
           return $this->draw('manage.html', ['sub_modules' => $sub_modules]);
         }
 
-        /* Start Bahasa Section */
         public function getBridgingSEP()
         {
-          $this->core->addJS(url([ADMIN, 'laporan', 'bridgingsepjs']), 'footer');
-          $return = $this->bahasa->getIndex();
-          return $this->draw('bridgingsep.html', [
-            'bahasa' => $return
-          ]);
+          $this->_addHeaderFiles();
+          $settings = $this->settings('settings');
+          $this->tpl->set('settings', $this->tpl->noParse_array(htmlspecialchars_array($settings)));
 
-        }
+          $tgl_awal = date('Y-m-d');
+          $tgl_akhir = date('Y-m-d');
 
-        public function anyBahasaForm()
-        {
-            $return = $this->bahasa->anyForm();
-            echo $this->draw('bahasa.form.html', ['bahasa' => $return]);
+          if(isset($_GET['tgl_awal'])) {
+            $tgl_awal = $_GET['tgl_awal'];
+          }
+          if(isset($_GET['tgl_akhir'])) {
+            $tgl_akhir = $_GET['tgl_akhir'];
+          }
+
+          $sql = "SELECT * FROM bridging_sep WHERE (tglsep BETWEEN '$tgl_awal' AND '$tgl_akhir')";
+
+          $stmt = $this->db()->pdo()->prepare($sql);
+          $stmt->execute();
+          $rows = $stmt->fetchAll();
+
+          $return['list'] = [];
+          $i = 1;
+          foreach ($rows as $row) {
+            $row['nomor'] = $i++;
+            $return['list'][] = $row;
+          }
+
+          if(isset($_GET['action']) && $_GET['action'] == 'print') {
+            echo $this->draw('bridgingsep.print.html', [
+              'bridgingsep' => $return,
+              'action' => url([ADMIN,'laporan','bridgingsep'])
+            ]);
             exit();
+          } else {
+            return $this->draw('bridgingsep.html', [
+              'bridgingsep' => $return,
+              'action' => url([ADMIN,'laporan','bridgingsep'])
+            ]);
+          }
         }
-
-        public function anyBahasaDisplay()
-        {
-            $return = $this->bahasa->anyDisplay();
-            echo $this->draw('bahasa.display.html', ['bahasa' => $return]);
-            exit();
-        }
-
-        public function postBahasaSave()
-        {
-          $this->bahasa->postSave();
-          exit();
-        }
-
-        public function postBahasaHapus()
-        {
-          $this->bahasa->postHapus();
-          exit();
-        }
-
-        public function getBahasaJS()
-        {
-            header('Content-type: text/javascript');
-            echo $this->draw(MODULES.'/master/js/admin/bahasa.js');
-            exit();
-        }
-        /* End Bahasa Section */
 
         public function getCSS()
         {
             header('Content-type: text/css');
-            echo $this->draw(MODULES.'/master/css/admin/master.css');
+            echo $this->draw(MODULES.'/laporan/css/admin/laporan.css');
             exit();
         }
 
         public function getJavascript()
         {
             header('Content-type: text/javascript');
-            echo $this->draw(MODULES.'/master/js/admin/master.js');
+            echo $this->draw(MODULES.'/laporan/js/admin/laporan.js');
             exit();
         }
 
         private function _addHeaderFiles()
         {
             // CSS
+            $this->core->addCSS(url('assets/css/dataTables.bootstrap.min.css'));
+            $this->core->addJS(url('assets/jscripts/jquery.dataTables.min.js'));
+            $this->core->addJS(url('assets/jscripts/dataTables.bootstrap.min.js'));
             $this->core->addCSS(url('assets/css/bootstrap-datetimepicker.css'));
             $this->core->addJS(url('assets/jscripts/moment-with-locales.js'));
             $this->core->addJS(url('assets/jscripts/bootstrap-datetimepicker.js'));
 
             // MODULE SCRIPTS
-            $this->core->addCSS(url([ADMIN, 'master', 'css']));
-            $this->core->addJS(url([ADMIN, 'master', 'javascript']), 'footer');
+            $this->core->addCSS(url([ADMIN, 'laporan', 'css']));
+            $this->core->addJS(url([ADMIN, 'laporan', 'javascript']), 'footer');
         }
 
     }
