5eae778372c23f279e914433ef039ee233cb50b9
basoro <dentix.id@gmail.com>
2021-01-12 17:58:13 +0800

    Add module Dokter IGD
    Catatan perubahan
    - Refractoring ke versi baru 2021
    - Pilhan tema admin
    - Support mobile APPs via PWA
    - Perbaikan modul Kasir Ralan
    - Perbaikan modul Apotik Ralan
    - Perbaikan modul pengaturan
    - Perbaikan modul dokter rawat jalan
    - Add modul laporan
    - Add modul dokter rawat inap
    - Add modul dokter IGD
    - Dan masih banyak lagi

---
+++ plugins/dokter_igd/Info.php
@@ -0,0 +1,15 @@
+<?php
+
+    return [
+        'name'          =>  'Dokter IGD',
+        'description'   =>  'Modul dokter IGD untuk KhanzaLITE',
+        'author'        =>  'Basoro',
+        'version'       =>  '1.0',
+        'compatibility' =>  '2021',
+        'icon'          =>  'user-md',
+        'install'       =>  function () use ($core) {
+        },
+        'uninstall'     =>  function() use($core)
+        {
+        }
+    ];
