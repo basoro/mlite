2ac04c99daafa26a131798914f8018c1f97bcc97
basoro <dentix.id@gmail.com>
2021-01-30 21:37:31 +0800

    Update pasien.js

--- plugins/pasien/js/admin/pasien.js
+++ plugins/pasien/js/admin/pasien.js
@@ -113,21 +113,19 @@ $("#form").on("click", "#simpan", function(event){
       kd_pj: kd_pj,
       no_peserta: no_peserta
     } ,function(data) {
-      if(data.status == 'success'){
-        $("#display").show().load(baseURL + '/pasien/display?t=' + mlite.token);
-        $("#form").hide();
-        $("#tutupform").val("Buka Form");
-        $("#tutupform").attr("id", "bukaform");
-        $('#notif').html("<div class=\"alert alert-success alert-dismissible fade in\" role=\"alert\" style=\"border-radius:0px;margin-top:-15px;\">"+
-        "Data pasien telah disimpan!"+
-        "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">&times;</button>"+
-        "</div>").show();
-      } else {
-        $('#notif').html("<div class=\"alert alert-danger alert-dismissible fade in\" role=\"alert\" style=\"border-radius:0px;margin-top:-15px;\">"+
-        "Gagal menyimpan data pasien!"+
-        "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">&times;</button>"+
-        "</div>").show();
-      }
+      $("#display").show().load(baseURL + '/pasien/display?t=' + mlite.token);
+      $("#form").hide();
+      $("#tutupform").val("Buka Form");
+      $("#tutupform").attr("id", "bukaform");
+      $('#notif').html("<div class=\"alert alert-success alert-dismissible fade in\" role=\"alert\" style=\"border-radius:0px;margin-top:-15px;\">"+
+      "Data pasien telah disimpan!"+
+      "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">&times;</button>"+
+      "</div>").show();
+    }).error(function () {
+      $('#notif').html("<div class=\"alert alert-danger alert-dismissible fade in\" role=\"alert\" style=\"border-radius:0px;margin-top:-15px;\">"+
+      "Gagal menyimpan data pasien!"+
+      "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">&times;</button>"+
+      "</div>").show();
     });
   }
 
