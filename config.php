fc584dae62cb2793912281608c702378a0dbda18
basoro <dentix.id@gmail.com>
2021-04-13 09:05:16 +0800

    Update config.php

--- config.php
+++ config.php
@@ -6,8 +6,8 @@ if (!version_compare(PHP_VERSION, '5.5.0', '>=')) {
 define('DBHOST', 'localhost');
 define('DBPORT', '3306');
 define('DBUSER', 'root');
-define('DBPASS', 'basoro');
-define('DBNAME', 'mlite');
+define('DBPASS', '');
+define('DBNAME', 'sik');
 
 // URL Webapps
 define('WEBAPPS_URL', 'http://localhost/webapps');
