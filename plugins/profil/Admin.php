705c2a6cc8cd2f1d319465b807caaebf3a041be9
basoro <dentix.id@gmail.com>
2021-06-16 20:48:51 +0800

    Update

--- plugins/profil/Admin.php
+++ plugins/profil/Admin.php
@@ -28,7 +28,12 @@ class Admin extends AdminModule
         ['name' => 'Jadwal', 'url' => url([ADMIN, 'profil', 'jadwal']), 'icon' => 'cubes', 'desc' => 'Jadwal Pegawai'],
         ['name' => 'Ganti Password', 'url' => url([ADMIN, 'profil', 'ganti_pass']), 'icon' => 'cubes', 'desc' => 'Ganti Pasword'],
       ];
-      return $this->draw('manage.html', ['sub_modules' => $sub_modules]);
+      $username = $this->core->getUserInfo('username', null, true);
+      $profil = $this->db('pegawai')->where('nik',$username)->oneArray();
+      $tanggal = getDayIndonesia(date('Y-m-d')).', '.dateIndonesia(date('Y-m-d'));
+      $presensi = $this->db('rekap_presensi')->where('id',$profil['id'])->where('photo', '!=', '')->like('jam_datang', date('Y-m-d').'%')->toArray();
+      $absensi = $this->db('rekap_presensi')->where('id',$profil['id'])->where('photo', '')->like('jam_datang', date('Y-m-d').'%')->toArray();
+      return $this->draw('manage.html', ['sub_modules' => $sub_modules, 'profil' => $profil, 'tanggal' => $tanggal, 'presensi' => $presensi, 'absensi' => $absensi]);
     }
 
     public function getBiodata()
