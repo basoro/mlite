5cdffc2dacfa561f45f9ad56d9b99eb9c37f1ba7
adlylee <adityahidayat@gmail.com>
2021-06-23 10:21:11 +0800

    fix absen rigid

--- plugins/dashboard/Admin.php
+++ plugins/dashboard/Admin.php
@@ -117,9 +117,11 @@ class Admin extends AdminModule
               $urlnya         = WEBAPPS_URL.'/presensi/'.$gambar;
               $barcode        = $this->core->getUserInfo('username', null, true);
 
+              $bulan = date('m');
+              $tahun = date('y');
               $idpeg          = $this->db('barcode')->where('barcode', $barcode)->oneArray();
               $jam_jaga       = $this->db('jam_jaga')->join('pegawai', 'pegawai.departemen = jam_jaga.dep_id')->where('pegawai.id', $idpeg['id'])->where('jam_jaga.shift', $_GET['shift'])->oneArray();
-              $jadwal_pegawai = $this->db('jadwal_pegawai')->where('id', $idpeg['id'])->where('h'.date('j'), $_GET['shift'])->oneArray();
+              $jadwal_pegawai = $this->db('jadwal_pegawai')->where('id', $idpeg['id'])->where('h'.date('j'), $_GET['shift'])->where('bulan', $bulan)->where('tahun', $tahun)->oneArray();
 
               $set_keterlambatan  = $this->db('set_keterlambatan')->toArray();
               $toleransi      = $set_keterlambatan['toleransi'];
