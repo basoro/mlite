169db246f79fc4b42c361139be5be06dd4f3cf75
basoro <dentix.id@gmail.com>
2022-03-10 08:10:16 +0800

    Update wa api di billing ralan

--- plugins/api/Admin.php
+++ plugins/api/Admin.php
@@ -240,6 +240,40 @@ class Admin extends AdminModule
         exit();
     }
 
+    public function postKirimWADocument()
+    {
+        $data = [
+            'api_key' => $_POST['api_key'],
+            'sender'  => $_POST['sender'],
+            'number'  => $_POST['number'],
+            'message' => $_POST['message'],
+            'filetype' => $_POST['tipe'],
+            'url' => $_POST['file']
+        ];
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => "https://banoewa.com/send-document",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "POST",
+          CURLOPT_POSTFIELDS => json_encode($data),
+          CURLOPT_HTTPHEADER => array(
+             'Content-Type: application/json'
+           ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+        exit();
+    }
+
     public function getPaymentDuitku()
     {
 
