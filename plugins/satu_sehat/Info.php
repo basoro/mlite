dc2239bc1a852bc8f6f85a4ec488a3a385799bc2
Basoro <dentix.id@gmail.com>
2023-12-02 07:42:38 +0800

    Update Info.php

--- plugins/satu_sehat/Info.php
+++ plugins/satu_sehat/Info.php
@@ -5,7 +5,7 @@ return [
   'description'   =>  'Modul Satu Sehat Kemkes',
   'author'        =>  'Basoro',
   'version'       =>  '1.0',
-  'compatibility' =>  '2022',
+  'compatibility' =>  '2023',
   'icon'          =>  'heart',
   'install'       =>  function () use ($core) {
     $core->db()->pdo()->exec("INSERT INTO `mlite_settings` (`module`, `field`, `value`) VALUES ('satu_sehat', 'organizationid', '')");
