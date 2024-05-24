09a95113e4c103aa280ab38e8c31c2254a41b639
Basoro <dentix.id@gmail.com>
2024-05-24 20:51:26 +0800

    Update

---
+++ plugins/detail_pemberian_obat/Info.php
@@ -0,0 +1,15 @@
+<?php
+
+    return [
+        'name'          =>  'Detail Pemberian Obat',
+        'description'   =>  'Modul detail pemberian obat untuk mLITE',
+        'author'        =>  'Basoro',
+        'version'       =>  '1.0',
+        'compatibility' =>  '4.0.*',
+        'icon'          =>  'code',
+        'install'       =>  function () use ($core) {
+        },
+        'uninstall'     =>  function() use($core)
+        {
+        }
+    ];
