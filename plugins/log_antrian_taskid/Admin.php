68b2c48e66c39680d719dd74e9aebeffefd81793
Basoro <dentix.id@gmail.com>
2024-09-02 10:48:23 +0800

    Add log antrian

---
+++ plugins/log_antrian_taskid/Admin.php
@@ -0,0 +1,207 @@
+<?php
+namespace Plugins\Log_Antrian_TaskID;
+
+use Systems\AdminModule;
+
+class Admin extends AdminModule
+{
+
+    public function navigation()
+    {
+        return [
+            'Kelola'   => 'manage',
+        ];
+    }
+
+    public function getManage(){
+        $this->_addHeaderFiles();
+        return $this->draw('manage.html');
+    }
+
+    public function postData(){
+        $draw = $_POST['draw'];
+        $row1 = $_POST['start'];
+        $rowperpage = $_POST['length']; // Rows display per page
+        $columnIndex = $_POST['order'][0]['column']; // Column index
+        $columnName = $_POST['columns'][$columnIndex]['data']; // Column name
+        $columnSortOrder = $_POST['order'][0]['dir']; // asc or desc
+        $searchValue = $_POST['search']['value']; // Search value
+
+        ## Custom Field value
+        $search_field_mlite_antrian_referensi_taskid= $_POST['search_field_mlite_antrian_referensi_taskid'];
+        $search_text_mlite_antrian_referensi_taskid = $_POST['search_text_mlite_antrian_referensi_taskid'];
+
+        $searchQuery = " ";
+        if($search_text_mlite_antrian_referensi_taskid != ''){
+            $searchQuery .= " and (".$search_field_mlite_antrian_referensi_taskid." like '%".$search_text_mlite_antrian_referensi_taskid."%' ) ";
+        }
+
+        $conn = new \PDO("mysql:host=192.168.0.2;port=3306;dbname=rshd_sik", "rshd_sik", "Murakata04");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // $this->db()->pdo() = $conn;
+
+        ## Total number of records without filtering
+        $sel = $conn->prepare("select count(*) as allcount from mlite_antrian_referensi_taskid");
+        $sel->execute();
+        $records = $sel->fetch();
+        $totalRecords = $records['allcount'];
+
+        ## Total number of records with filtering
+        $sel = $conn->prepare("select count(*) as allcount from mlite_antrian_referensi_taskid WHERE 1 ".$searchQuery);
+        $sel->execute();
+        $records = $sel->fetch();
+        $totalRecordwithFilter = $records['allcount'];
+
+        ## Fetch records
+        $sel = $conn->prepare("select * from mlite_antrian_referensi_taskid WHERE 1 ".$searchQuery." order by ".$columnName." ".$columnSortOrder." limit ".$row1.",".$rowperpage);
+        $sel->execute();
+        $result = $sel->fetchAll(\PDO::FETCH_ASSOC);
+
+        $data = array();
+        foreach($result as $row) {
+            $data[] = array(
+                'tanggal_periksa'=>$row['tanggal_periksa'],
+'nomor_referensi'=>$row['nomor_referensi'],
+'taskid'=>$row['taskid'],
+'waktu'=>$row['waktu'],
+'status'=>$row['status'],
+'keterangan'=>$row['keterangan']
+
+            );
+        }
+
+        ## Response
+        $response = array(
+            "draw" => intval($draw), 
+            "iTotalRecords" => $totalRecords,
+            "iTotalDisplayRecords" => $totalRecordwithFilter,
+            "aaData" => $data
+        );
+
+        echo json_encode($response);
+        exit();
+    }
+
+    public function postAksi()
+    {
+        if(isset($_POST['typeact'])){ 
+            $act = $_POST['typeact']; 
+        }else{ 
+            $act = ''; 
+        }
+
+        if ($act=='add') {
+
+        $tanggal_periksa = $_POST['tanggal_periksa'];
+        $nomor_referensi = $_POST['nomor_referensi'];
+        $taskid = $_POST['taskid'];
+        $waktu = $_POST['waktu'];
+        $status = $_POST['status'];
+        $keterangan = $_POST['keterangan'];
+
+            
+            $mlite_antrian_referensi_taskid_add = $this->db()->pdo()->prepare('INSERT INTO mlite_antrian_referensi_taskid VALUES (?, ?, ?, ?, ?, ?)');
+            $mlite_antrian_referensi_taskid_add->execute([$tanggal_periksa, $nomor_referensi, $taskid, $waktu, $status, $keterangan]);
+
+        }
+        if ($act=="edit") {
+
+        $tanggal_periksa = $_POST['tanggal_periksa'];
+        $nomor_referensi = $_POST['nomor_referensi'];
+        $taskid = $_POST['taskid'];
+        $waktu = $_POST['waktu'];
+        $status = $_POST['status'];
+        $keterangan = $_POST['keterangan'];
+
+
+        // BUANG FIELD PERTAMA
+
+            $mlite_antrian_referensi_taskid_edit = $this->db()->pdo()->prepare("UPDATE mlite_antrian_referensi_taskid SET tanggal_periksa=?, nomor_referensi=?, taskid=?, waktu=?, status=?, keterangan=? WHERE tanggal_periksa=?");
+            $mlite_antrian_referensi_taskid_edit->execute([$tanggal_periksa, $nomor_referensi, $taskid, $waktu, $status, $keterangan,$tanggal_periksa]);
+        
+        }
+
+        if ($act=="del") {
+            $tanggal_periksa= $_POST['tanggal_periksa'];
+            $check_db = $this->db()->pdo()->prepare("DELETE FROM mlite_antrian_referensi_taskid WHERE tanggal_periksa='$tanggal_periksa'");
+            $result = $check_db->execute();
+            $error = $check_db->errorInfo();
+            if (!empty($result)){
+              $data = array(
+                'status' => 'success', 
+                'msg' => $no_rkm_medis
+              );
+            } else {
+              $data = array(
+                'status' => 'error', 
+                'msg' => $error['2']
+              );
+            }
+            echo json_encode($data);                    
+        }
+
+        if ($act=="lihat") {
+
+            $search_field_mlite_antrian_referensi_taskid= $_POST['search_field_mlite_antrian_referensi_taskid'];
+            $search_text_mlite_antrian_referensi_taskid = $_POST['search_text_mlite_antrian_referensi_taskid'];
+
+            $searchQuery = " ";
+            if($search_text_mlite_antrian_referensi_taskid != ''){
+                $searchQuery .= " and (".$search_field_mlite_antrian_referensi_taskid." like '%".$search_text_mlite_antrian_referensi_taskid."%' ) ";
+            }
+
+            $user_lihat = $this->db()->pdo()->prepare("SELECT keterangan, count('keterangan') as jumlah from mlite_antrian_referensi_taskid WHERE 1 $searchQuery group by keterangan order by jumlah desc");
+            $user_lihat->execute();
+            $result = $user_lihat->fetchAll(\PDO::FETCH_ASSOC);
+
+            $data = array();
+
+            foreach($result as $row) {
+                $data[] = array(
+                    'keterangan'=>$row['keterangan'],
+                    'jumlah'=>$row['jumlah']
+                );
+            }
+
+            echo json_encode($data);
+        }
+        exit();
+    }
+
+    public function getDetail($tanggal_periksa)
+    {
+        $detail = $this->db('mlite_antrian_referensi_taskid')->where('tanggal_periksa', $tanggal_periksa)->toArray();
+        echo $this->draw('detail.html', ['detail' => $detail]);
+        exit();
+    }
+
+    public function getCss()
+    {
+        header('Content-type: text/css');
+        echo $this->draw(MODULES.'/log_antrian_taskid/css/admin/styles.css');
+        exit();
+    }
+
+    public function getJavascript()
+    {
+        header('Content-type: text/javascript');
+        $settings = $this->settings('settings');
+        echo $this->draw(MODULES.'/log_antrian_taskid/js/admin/scripts.js', ['settings' => $settings]);
+        exit();
+    }
+
+    private function _addHeaderFiles()
+    {
+        $this->core->addCSS(url('plugins/log_antrian_taskid/css/admin/datatables.min.css'));
+        $this->core->addJS(url('plugins/log_antrian_taskid/js/admin/jqueryvalidation.js'));
+        $this->core->addJS(url('plugins/log_antrian_taskid/js/admin/xlsx.js'));
+        $this->core->addJS(url('plugins/log_antrian_taskid/js/admin/jspdf.min.js'));
+        $this->core->addJS(url('plugins/log_antrian_taskid/js/admin/jspdf.plugin.autotable.min.js'));
+        $this->core->addJS(url('plugins/log_antrian_taskid/js/admin/datatables.min.js'));
+
+        $this->core->addCSS(url([ADMIN, 'log_antrian_taskid', 'css']));
+        $this->core->addJS(url([ADMIN, 'log_antrian_taskid', 'javascript']), 'footer');
+    }
+
+}
---
+++ plugins/log_antrian_taskid/js/admin/scripts.js
@@ -0,0 +1,335 @@
+jQuery().ready(function () {
+    var var_tbl_mlite_antrian_referensi_taskid = $('#tbl_mlite_antrian_referensi_taskid').DataTable({
+        'processing': true,
+        'serverSide': true,
+        'serverMethod': 'post',
+        'dom': 'Bfrtip',
+        'searching': false,
+        'select': true,
+        'colReorder': true,
+        "bInfo" : false,
+        "ajax": {
+            "url": "{?=url([ADMIN,'log_antrian_taskid','data'])?}",
+            "dataType": "json",
+            "type": "POST",
+            "data": function (data) {
+
+                // Read values
+                var search_field_mlite_antrian_referensi_taskid = $('#search_field_mlite_antrian_referensi_taskid').val();
+                var search_text_mlite_antrian_referensi_taskid = $('#search_text_mlite_antrian_referensi_taskid').val();
+                
+                data.search_field_mlite_antrian_referensi_taskid = search_field_mlite_antrian_referensi_taskid;
+                data.search_text_mlite_antrian_referensi_taskid = search_text_mlite_antrian_referensi_taskid;
+                
+            }
+        },
+        "columns": [
+{ 'data': 'tanggal_periksa' },
+{ 'data': 'nomor_referensi' },
+{ 'data': 'taskid' },
+{ 'data': 'waktu' },
+{ 'data': 'status' },
+{ 'data': 'keterangan' }
+
+        ],
+        "columnDefs": [
+{ 'targets': 0},
+{ 'targets': 1},
+{ 'targets': 2},
+{ 'targets': 3},
+{ 'targets': 4},
+{ 'targets': 5}
+
+        ],
+        buttons: [],
+        "scrollCollapse": true,
+        // "scrollY": '48vh', 
+        "pageLength":'25', 
+        "lengthChange": true,
+        "scrollX": true,
+        dom: "<'row'<'col-sm-12'tr>><<'pmd-datatable-pagination' l i p>>"
+    });
+
+    // ==============================================================
+    // FORM VALIDASI
+    // ==============================================================
+
+    $("form[name='form_mlite_antrian_referensi_taskid']").validate({
+        rules: {
+tanggal_periksa: 'required',
+nomor_referensi: 'required',
+taskid: 'required',
+waktu: 'required',
+status: 'required',
+keterangan: 'required'
+
+        },
+        messages: {
+tanggal_periksa:'tanggal_periksa tidak boleh kosong!',
+nomor_referensi:'nomor_referensi tidak boleh kosong!',
+taskid:'taskid tidak boleh kosong!',
+waktu:'waktu tidak boleh kosong!',
+status:'status tidak boleh kosong!',
+keterangan:'keterangan tidak boleh kosong!'
+
+        },
+        submitHandler: function (form) {
+ var tanggal_periksa= $('#tanggal_periksa').val();
+var nomor_referensi= $('#nomor_referensi').val();
+var taskid= $('#taskid').val();
+var waktu= $('#waktu').val();
+var status= $('#status').val();
+var keterangan= $('#keterangan').val();
+
+ var typeact = $('#typeact').val();
+
+ var formData = new FormData(form); // tambahan
+ formData.append('typeact', typeact); // tambahan
+
+            $.ajax({
+                url: "{?=url([ADMIN,'log_antrian_taskid','aksi'])?}",
+                method: "POST",
+                contentType: false, // tambahan
+                processData: false, // tambahan
+                data: formData,
+                success: function (data) {
+                    if (typeact == "add") {
+                        alert("Data Berhasil Ditambah");
+                    }
+                    else if (typeact == "edit") {
+                        alert("Data Berhasil Diubah");
+                    }
+                    $("#modal_cs").hide();
+                    location.reload(true);
+                }
+            })
+        }
+    });
+
+    // ==============================================================
+    // KETIKA MENGETIK DI INPUT SEARCH
+    // ==============================================================
+    $('#search_text_mlite_antrian_referensi_taskid').keyup(function () {
+        var_tbl_mlite_antrian_referensi_taskid.draw();
+    });
+    // ==============================================================
+    // CLICK TANDA X DI INPUT SEARCH
+    // ==============================================================
+    $("#searchclear_mlite_antrian_referensi_taskid").click(function () {
+        $("#search_text_mlite_antrian_referensi_taskid").val("");
+        var_tbl_mlite_antrian_referensi_taskid.draw();
+    });
+
+    // ===========================================
+    // Ketika tombol Edit di tekan
+    // ===========================================
+
+    $("#edit_data_mlite_antrian_referensi_taskid").click(function () {
+        var rowData = var_tbl_mlite_antrian_referensi_taskid.rows({ selected: true }).data()[0];
+        if (rowData != null) {
+
+            var tanggal_periksa = rowData['tanggal_periksa'];
+var nomor_referensi = rowData['nomor_referensi'];
+var taskid = rowData['taskid'];
+var waktu = rowData['waktu'];
+var status = rowData['status'];
+var keterangan = rowData['keterangan'];
+
+
+
+            $("#typeact").val("edit");
+  
+            $('#tanggal_periksa').val(tanggal_periksa);
+$('#nomor_referensi').val(nomor_referensi);
+$('#taskid').val(taskid);
+$('#waktu').val(waktu);
+$('#status').val(status);
+$('#keterangan').val(keterangan);
+
+            //$("#tanggal_periksa").prop('disabled', true); // GA BISA DIEDIT KALI DISABLE
+            $('#modal-title').text("Edit Data Log Antrian TaskID");
+            $("#modal_mlite_antrian_referensi_taskid").modal();
+        }
+        else {
+            alert("Silakan pilih data yang akan di edit.");
+        }
+
+    });
+
+    // ==============================================================
+    // TOMBOL  DELETE DI CLICK
+    // ==============================================================
+    jQuery("#hapus_data_mlite_antrian_referensi_taskid").click(function () {
+        var rowData = var_tbl_mlite_antrian_referensi_taskid.rows({ selected: true }).data()[0];
+
+
+        if (rowData) {
+var tanggal_periksa = rowData['tanggal_periksa'];
+            var a = confirm("Anda yakin akan menghapus data dengan tanggal_periksa=" + tanggal_periksa);
+            if (a) {
+
+                $.ajax({
+                    url: "{?=url([ADMIN,'log_antrian_taskid','aksi'])?}",
+                    method: "POST",
+                    data: {
+                        tanggal_periksa: tanggal_periksa,
+                        typeact: 'del'
+                    },
+                    success: function (data) {
+                        data = JSON.parse(data);
+                        if(data.status === 'success') {
+                            alert(data.msg);
+                        } else {
+                            alert(data.msg);
+                        }
+                        location.reload(true);
+                    }
+                })
+            }
+        }
+        else {
+            alert("Pilih satu baris untuk dihapus");
+        }
+    });
+
+    // ==============================================================
+    // TOMBOL TAMBAH DATA DI CLICK
+    // ==============================================================
+    jQuery("#tambah_data_mlite_antrian_referensi_taskid").click(function () {
+
+        $('#tanggal_periksa').val('');
+$('#nomor_referensi').val('');
+$('#taskid').val('');
+$('#waktu').val('');
+$('#status').val('');
+$('#keterangan').val('');
+
+
+        $("#typeact").val("add");
+        $("#tanggal_periksa").prop('disabled', false);
+        
+        $('#modal-title').text("Tambah Data Log Antrian TaskID");
+        $("#modal_mlite_antrian_referensi_taskid").modal();
+    });
+
+    // ===========================================
+    // Ketika tombol lihat data di tekan
+    // ===========================================
+    $("#lihat_data_mlite_antrian_referensi_taskid").click(function () {
+
+        var search_field_mlite_antrian_referensi_taskid = $('#search_field_mlite_antrian_referensi_taskid').val();
+        var search_text_mlite_antrian_referensi_taskid = $('#search_text_mlite_antrian_referensi_taskid').val();
+
+        $.ajax({
+            url: "{?=url([ADMIN,'log_antrian_taskid','aksi'])?}",
+            method: "POST",
+            data: {
+                typeact: 'lihat', 
+                search_field_mlite_antrian_referensi_taskid: search_field_mlite_antrian_referensi_taskid, 
+                search_text_mlite_antrian_referensi_taskid: search_text_mlite_antrian_referensi_taskid
+            },
+            dataType: 'json',
+            success: function (res) {
+                var eTable = "<div class='table-responsive'><table id='tbl_lihat_mlite_antrian_referensi_taskid' class='table display dataTable' style='width:100%'><thead><th>Response</th><th>Jumlah</th></thead>";
+                for (var i = 0; i < res.length; i++) {
+                    eTable += "<tr>";
+                    eTable += '<td>' + res[i]['keterangan'] + '</td>';
+                    eTable += '<td>' + res[i]['jumlah'] + '</td>';
+                    eTable += "</tr>";
+                }
+                eTable += "</tbody></table></div>";
+                $('#forTable_mlite_antrian_referensi_taskid').html(eTable);
+            }
+        });
+
+        $('#modal-title').text("Lihat Data");
+        $("#modal_lihat_mlite_antrian_referensi_taskid").modal();
+    });
+
+    // ==============================================================
+    // TOMBOL DETAIL mlite_antrian_referensi_taskid DI CLICK
+    // ==============================================================
+    jQuery("#lihat_detail_mlite_antrian_referensi_taskid").click(function (event) {
+
+        var rowData = var_tbl_mlite_antrian_referensi_taskid.rows({ selected: true }).data()[0];
+
+        if (rowData) {
+var tanggal_periksa = rowData['tanggal_periksa'];
+            var baseURL = mlite.url + '/' + mlite.admin;
+            event.preventDefault();
+            var loadURL =  baseURL + '/log_antrian_taskid/detail/' + tanggal_periksa + '?t=' + mlite.token;
+        
+            var modal = $('#modal_detail_mlite_antrian_referensi_taskid');
+            var modalContent = $('#modal_detail_mlite_antrian_referensi_taskid .modal-content');
+        
+            modal.off('show.bs.modal');
+            modal.on('show.bs.modal', function () {
+                modalContent.load(loadURL);
+            }).modal();
+            return false;
+        
+        }
+        else {
+            alert("Pilih satu baris untuk detail");
+        }
+    });
+        
+    // ===========================================
+    // Ketika tombol export pdf di tekan
+    // ===========================================
+    $("#export_pdf").click(function () {
+
+        var doc = new jsPDF('p', 'pt', 'A4'); /* pilih 'l' atau 'p' */
+        var img = "{?=base64_encode(file_get_contents(url($settings['logo'])))?}";
+        doc.addImage(img, 'JPEG', 20, 10, 50, 50);
+        doc.setFontSize(20);
+        doc.text("{$settings.nama_instansi}", 80, 35, null, null, null);
+        doc.setFontSize(10);
+        doc.text("{$settings.alamat} - {$settings.kota} - {$settings.propinsi}", 80, 46, null, null, null);
+        doc.text("Telepon: {$settings.nomor_telepon} - Email: {$settings.email}", 80, 56, null, null, null);
+        doc.line(20,70,572,70,null); /* doc.line(20,70,820,70,null); --> Jika landscape */
+        doc.line(20,72,572,72,null); /* doc.line(20,72,820,72,null); --> Jika landscape */
+        doc.setFontSize(14);
+        doc.text("Tabel Data Mlite Antrian Referensi Taskid", 20, 95, null, null, null);
+        const totalPagesExp = "{total_pages_count_string}";        
+        doc.autoTable({
+            html: '#tbl_lihat_mlite_antrian_referensi_taskid',
+            startY: 105,
+            margin: {
+                left: 20, 
+                right: 20
+            }, 
+            styles: {
+                fontSize: 10,
+                cellPadding: 5
+            }, 
+            didDrawPage: data => {
+                let footerStr = "Page " + doc.internal.getNumberOfPages();
+                if (typeof doc.putTotalPages === 'function') {
+                footerStr = footerStr + " of " + totalPagesExp;
+                }
+                doc.setFontSize(10);
+                doc.text(footerStr, data.settings.margin.left, doc.internal.pageSize.height - 10);
+           }
+        });
+        if (typeof doc.putTotalPages === 'function') {
+            doc.putTotalPages(totalPagesExp);
+        }
+        // doc.save('table_data_mlite_antrian_referensi_taskid.pdf')
+        window.open(doc.output('bloburl'), '_blank',"toolbar=no,status=no,menubar=no,scrollbars=no,resizable=no,modal=yes");  
+              
+    })
+
+    // ===========================================
+    // Ketika tombol export xlsx di tekan
+    // ===========================================
+    $("#export_xlsx").click(function () {
+        let tbl1 = document.getElementById("tbl_lihat_mlite_antrian_referensi_taskid");
+        let worksheet_tmp1 = XLSX.utils.table_to_sheet(tbl1);
+        let a = XLSX.utils.sheet_to_json(worksheet_tmp1, { header: 1 });
+        let worksheet1 = XLSX.utils.json_to_sheet(a, { skipHeader: true });
+        const new_workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(new_workbook, worksheet1, "Data mlite_antrian_referensi_taskid");
+        XLSX.writeFile(new_workbook, 'tmp_file.xls');
+    })
+});
