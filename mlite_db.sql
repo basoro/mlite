ef3550b38c77496ca0c00469c87a8b8cb4d2274f
Basoro <dentix.id@gmail.com>
2025-01-19 17:14:06 +0800

    Update version 4.1.9

--- mlite_db.sql
+++ mlite_db.sql
@@ -3063,7 +3063,7 @@ VALUES
 	(31,'settings','admin_mode','complex'),
 	(32,'settings','input_kasir','tidak'),
 	(33,'settings','editor','wysiwyg'),
-	(34,'settings','version','4.1.8'),
+	(34,'settings','version','4.1.9'),
 	(35,'settings','update_check','0'),
 	(36,'settings','update_changelog',''),
 	(37,'settings','update_version','0'),
