36e87ee47641ae678a0662c154ceff019e94d5bd
Basoro <dentix.id@gmail.com>
2025-12-29 19:41:34 +0800

    Update

--- plugins/rawat_inap/Admin.php
+++ plugins/rawat_inap/Admin.php
@@ -2356,6 +2356,9 @@ class Admin extends AdminModule
                 ->oneArray()) {
                 $this->db('pemeriksaan_ranap')->save($data);
               } else {
+                unset($data['no_rawat']);
+                unset($data['tgl_perawatan']);
+                unset($data['jam_rawat']);
                 $this->db('pemeriksaan_ranap')
                     ->where('no_rawat', $data['no_rawat'])
                     ->where('tgl_perawatan', $data['tgl_perawatan'])
