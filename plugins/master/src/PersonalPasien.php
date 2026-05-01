f495374b29ef937fd691129f18783422d3ebe0d1
Basoro <dentix.id@gmail.com>
2026-05-01 21:06:59 +0800

    feat(personal_pasien): add photo upload and processing for patient profiles
    Add support for uploading patient photos via file input or webcam. The image is cropped to a square and resized to a maximum of 512x512 pixels. Old photos are automatically removed when a new one is uploaded. Also sets a default password for new patient records.

--- plugins/master/src/PersonalPasien.php
+++ plugins/master/src/PersonalPasien.php
@@ -17,11 +17,63 @@ class PersonalPasien
             return false;
         }
 
-        if (!$this->db('personal_pasien')->where('no_rkm_medis', $_POST['no_rkm_medis'])->oneArray()) {
-            return $this->db('personal_pasien')->save($_POST);
+        $personalPasien = $this->db('personal_pasien')
+            ->where('no_rkm_medis', $_POST['no_rkm_medis'])
+            ->oneArray();
+
+        $gambar = null;
+        $img = null;
+        $photo = isset_or($_FILES['file']['tmp_name'], false);
+        if (!$photo) {
+            $photo = isset_or($_FILES['webcam']['tmp_name'], false);
         }
 
-        return $this->db('personal_pasien')->where('no_rkm_medis', $_POST['no_rkm_medis'])->save($_POST);
+        if ($photo) {
+            $img = new \Systems\Lib\Image();
+            if ($img->load($photo)) {
+                if ($img->getInfos('width') < $img->getInfos('height')) {
+                    $img->crop(0, 0, (int) $img->getInfos('width'), (int) $img->getInfos('width'));
+                } else {
+                    $img->crop(0, 0, (int) $img->getInfos('height'), (int) $img->getInfos('height'));
+                }
+
+                if ($img->getInfos('width') > 512) {
+                    $img->resize(512, 512);
+                }
+
+                $gambar = 'pages/upload/' . uniqid('photo') . '.' . $img->getInfos('type');
+                $_POST['gambar'] = $gambar;
+            }
+        }
+
+        if (!$personalPasien) {
+            if (empty($_POST['password'])) {
+                $_POST['password'] = $_POST['no_rkm_medis'];
+            }
+            $query = $this->db('personal_pasien')->save($_POST);
+        } else {
+            $query = $this->db('personal_pasien')
+                ->where('no_rkm_medis', $_POST['no_rkm_medis'])
+                ->save($_POST);
+        }
+
+        if (
+            $query &&
+            $img &&
+            $gambar &&
+            $img->getInfos('width')
+        ) {
+            if (!empty($personalPasien['gambar'])) {
+                $oldPath = WEBAPPS_PATH . '/photopasien/' . $personalPasien['gambar'];
+                if (is_file($oldPath)) {
+                    unlink($oldPath);
+                }
+            }
+
+            $img->save(WEBAPPS_PATH . '/photopasien/' . $gambar);
+        }
+
+        return $query;
     }
 
     public function postHapus()
